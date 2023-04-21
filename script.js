1a70770cd01ca80b9f8f488c5f056687b1492caf
vaibhavchobisa <vaibhav.chobisa@gmail.com>
2023-04-21 15:37:30 +0530

    added logic for sound play when clicked

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-function playSound(e) {
+function playSoundByKeyboard(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!key) return;
     audio.currentTime = 0;
     audio.play();
     key.classList.add('playing');
+}
+
+function playSoundByClick(e) {
+    if(e.srcElement.nodeName==='KBD' || e.srcElement.nodeName==='SPAN') {
+        const audio = document.querySelector(`audio[data-key="${e.srcElement.parentElement.attributes[0].nodeValue}"]`);
+        const key = document.querySelector(`.key[data-key="${e.srcElement.parentElement.attributes[0].nodeValue}"]`);
+        if (!key) return;
+        audio.currentTime = 0;
+        audio.play();
+        key.classList.add('playing');
+    }
+    else if(e.srcElement.nodeName==='DIV') {
+        const audio = document.querySelector(`audio[data-key="${e.srcElement.attributes[0].nodeValue}"]`);
+        const key = document.querySelector(`.key[data-key="${e.srcElement.attributes[0].nodeValue}"]`);
+        if (!key) return;
+        audio.currentTime = 0;
+        audio.play();
+        key.classList.add('playing');
+    }
 }
 
 function removeTransition(e) {
@@ -15,4 +34,6 @@ function removeTransition(e) {
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
-window.addEventListener('keydown', playSound);
+window.addEventListener('keydown', playSoundByKeyboard);
+// for clicks :-
+keys.forEach(key => key.addEventListener('click', playSoundByClick));
