401a5c8d09d8dc7ad2668c29d9bbbdf2347b0a5c
vaibhavchobisa <vaibhav.chobisa@gmail.com>
2023-04-21 14:07:37 +0530

    removing if condition to avoid button sticking on continuous press

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function playSound(e) {
 }
 
 function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
+    // if (e.propertyName !== 'transform') return;
     this.classList.remove('playing');
 }
 
